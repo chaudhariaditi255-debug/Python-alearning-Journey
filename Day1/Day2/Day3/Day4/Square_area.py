c87506fc929346d4ae493cb7f699aee2f8480336
chaudhariaditi255-debug <chaudhariaditi255@gmail.com>
2026-06-21 18:02:25 +0530

    Add Square_area.py to calculate square area

---
+++ Day1/Day2/Day3/Day4/Square_area.py
@@ -0,0 +1,11 @@
+// Area of Square 
+def area(length):
+    return length*length
+
+length = float(input("Enter the length of the square: "))
+
+result = area(length)
+
+print("Area of Square:",result) 
+
+            
