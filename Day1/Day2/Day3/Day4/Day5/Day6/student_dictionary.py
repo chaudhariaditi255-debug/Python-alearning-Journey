564996048cf1f1eff15f2e0f73ffc98af85a8e1a
chaudhariaditi255-debug <chaudhariaditi255@gmail.com>
2026-06-29 22:09:15 +0530

    Create student_dictionary.py

---
+++ Day1/Day2/Day3/Day4/Day5/Day6/student_dictionary.py
@@ -0,0 +1,12 @@
+student = {
+    "name": "Aditi",
+    "age": 20,
+    "college": "GTU",
+    "branch": "Computer Engineering"
+}
+
+print("Student Details:")
+
+for key,value in student.items():
+    print(key,":", value)
+
